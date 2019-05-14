f107eaee677a9b17a686553dfe734b14b2d41da0
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-05-14 13:14:45 +0100

    proptype of product done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
 import classes from "./Product.css";
 import { NavLink } from "react-router-dom";
-
+import PropTypes from "prop-types";
 export default class Product extends Component {
   render() {
     const { id, title, img, price, inCart } = this.props.product;
-    console.log(title);
     return (
-      
-        <div className={classes.Card}>
-          <div className={classes.ImgContainer}>
-           <img src={img} alt={title} className={classes.Img} />
+      <div className={classes.Card}>
+        <NavLink to="/details">
+          <div
+            className={classes.ImgContainer}
+            onClick={() => console.log("you clicked me on the image container")}
+          >
+            <img src={img} alt={title} className={classes.Img} />
           </div>
           <h4 className={classes.Title}>{title}</h4>
-        </div>
-      
+        </NavLink>
+        <span className={classes.Price}>£ {price}</span>
+        <button
+          disabled={inCart ? true : false}
+          onClick={() => console.log("added to cart")}
+          className={inCart ? classes.CartBtnDisable : classes.CartBtn}
+        >
+          {inCart ? <span>in Cart</span> : <span>ADD To CART</span>}
+        </button>
+      </div>
     );
   }
+}
+
+Product.propTypes ={
+  product:PropTypes.shape({
+    id:PropTypes.number,
+    img:PropTypes.string,
+    title:PropTypes.string,
+    price:PropTypes.number,
+    inCart:PropTypes.bool
+  }).isRequired
 }
