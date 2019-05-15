840dfb1c3c970becb49ea645510a29294c3f7061
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-05-15 15:55:44 +0100

    incart button display componet completed

--- src/context.js
+++ src/context.js
@@ -6,7 +6,8 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
   state = {
     products: [],
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart:[]
   };
   componentDidMount() {
     this.setProduct();
@@ -25,14 +26,23 @@ class ProductProvider extends Component {
     const product = this.state.products.find(item => item.id === id);
     return product;
   };
-  handleDetail = (id) => {
-   const product = this.getItem(id);
-   this.setState(()=>{
-     return {detailProduct:product}
-   })
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
   addToCart = id => {
-    console.log("hello from add to cart", id);
+    let tempProduct = [...this.state.products];
+    const index = tempProduct.indexOf(this.getItem(id));
+    const product = tempProduct[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(()=>{
+      return {products:tempProduct ,cart:[...this.state.cart,product]}
+    },()=>{console.log(this.state);})
   };
   render() {
     return (
