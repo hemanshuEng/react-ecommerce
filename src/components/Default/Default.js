4131ea911594c1e5b0100de5e524ffefda8f45b3
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-05-16 14:52:22 +0100

    404 page created

--- src/components/Default/Default.js
+++ src/components/Default/Default.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-
+import classes from "./Default.css";
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>page not found</h3>
+      <div className={classes.Container}>
+        <h1>404</h1>
+        <h1>Error</h1>
+        <h2>page not found</h2>
+        <h3>Requested URL <strong>{this.props.location.pathname}</strong> was not found</h3>
       </div>
     );
   }
