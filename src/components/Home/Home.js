047412e709bff4dae1049368b67a793bed14b34d
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-05-17 21:08:22 +0100

    home page completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import classes from "./Home.css";
+import Luggage from "./../../assets/img/luggage.jpg";
+import { NavLink } from "react-router-dom";
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h3>hello from home</h3>
-        
+      <div className={classes.Container}>
+        <img src={Luggage} alt="home" className={classes.Img} />
+        <div className={classes.Overlay} />
+        <div className={classes.Headding}>
+          <h1 className={classes.Title}>
+            Thoughtgful standards for modern travel
+          </h1>
+          <NavLink to ="/shop">
+            <button className={classes.Btn}>shop now</button>
+          </NavLink>
+        </div>
       </div>
-    )
+    );
   }
 }
