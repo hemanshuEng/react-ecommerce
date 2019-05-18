7e25efcaf7224ed400e9ebfd2e4c0cda98afeb22
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-05-18 12:57:37 +0100

    typo mistake

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
         <div className={classes.Overlay} />
         <div className={classes.Headding}>
           <h1 className={classes.Title}>
-            Thoughtgful standards for modern travel
+            Thoughtful standards for modern travel
           </h1>
           <NavLink to ="/shop">
             <button className={classes.Btn}>shop now</button>
